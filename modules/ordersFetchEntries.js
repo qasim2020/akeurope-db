76bc2c0aa434968953beb48d3c2a76b9db1e6a48
Qasim Ali <qasimali24@gmail.com>
2025-01-16 15:54:41 +0100

    i think invoice generation is working good

--- modules/ordersFetchEntries.js
+++ modules/ordersFetchEntries.js
@@ -207,129 +207,218 @@ const getPreviousOrdersForEntry = async (entryId, orderId) => {
     return lastPaid;
 };
 
+const getNonFullySubscribedEntries = async (
+    orderId,
+    project,
+    alreadySelectedEntries,
+    searchQuery,
+) => {
+    const subscriptionFields = project.fields
+        .filter((field) => field.subscription === true)
+        .map((field) => field.name);
+
+    const DynamicModel = await createDynamicModel(project.slug);
+
+    const entries = await DynamicModel.find({
+        _id: {
+            $in: alreadySelectedEntries,
+            ...searchQuery,
+        },
+    }).lean();
+
+    const nonFullySubscribed = [];
+    const fullySubscribed = [];
+
+    for (const entry of entries) {
+        const requiredSubscriptions = subscriptionFields.filter(
+            (field) => entry[field] && entry[field] > 0,
+        );
+
+        if (requiredSubscriptions.length === 0) {
+            continue;
+        }
+
+        const currentDate = new Date();
+
+        const orders = await Order.find({
+            _id: { $ne: orderId },
+            'projects.entries.entryId': entry._id,
+            'projects.entries.totalCost': { $gt: 0 },
+            status: 'paid',
+        }).lean();
+
+        let isFullySubscribed = false;
+        let activeSubscriptions = [];
+        let costArray = [];
+        let tempReturnDoc = [];
+
+        for (const order of orders) {
+            let orderExpired = false;
+
+            const projectInOrder = order.projects.find(
+                (temp) => temp.slug === project.slug,
+            );
+
+            if (!projectInOrder) {
+                throw new Error('project not found something is wrong');
+            }
+
+            const expirationDate = new Date(order.createdAt);
+            expirationDate.setMonth(
+                expirationDate.getMonth() + projectInOrder.months + 1,
+            );
+            if (expirationDate <= currentDate) {
+                orderExpired = true;
+                break;
+            }
+
+            const projectEntries = projectInOrder.entries.filter(
+                (projEntry) =>
+                    projEntry.entryId.toString() === entry._id.toString(),
+            );
+            if (projectEntries.length === 0) break;
+
+            if (order.orderNo == 266 || entry.name == 'Dustin Guzman') {
+            }
+
+            for (const projEntry of projectEntries) {
+                for (const sub of requiredSubscriptions) {
+                    const cost = projEntry.costs.find(
+                        (field) => field.fieldName === sub,
+                    );
+                    if (cost.totalOrderedCost > 0) {
+                        activeSubscriptions.push(sub);
+                        costArray.push({
+                            cost,
+                            orderId: order._id,
+                        });
+                    } else {
+                        continue;
+                    }
+                    tempReturnDoc.push({
+                        orderId: order._id,
+                        selectedSubscriptions: projEntry.selectedSubscriptions,
+                        costs: projEntry.costs,
+                        expiry: expirationDate,
+                    });
+                    if (
+                        requiredSubscriptions.every((reqSub) =>
+                            activeSubscriptions.includes(reqSub),
+                        )
+                    ) {
+                        isFullySubscribed = true;
+                        break;
+                    }
+                }
+            }
+            if (isFullySubscribed) break;
+
+            if (orderExpired) break;
+            if (isFullySubscribed) break;
+        }
+
+        if (!isFullySubscribed) {
+            nonFullySubscribed.push({
+                entry,
+                entryId: entry._id,
+                entryName: entry.name,
+                activeSubscriptions,
+                requiredSubscriptions,
+                filteredCostArray: costArray,
+                oldOrders: tempReturnDoc,
+            });
+        } else {
+            fullySubscribed.push({
+                entry,
+                entryId: entry._id,
+                entryName: entry.name,
+                activeSubscriptions,
+                requiredSubscriptions,
+                filteredCostArray: costArray,
+                oldOrders: tempReturnDoc,
+            });
+        }
+    }
+
+    return { nonFullySubscribed, fullySubscribed };
+};
+
 const getOldestPaidEntries = async (req, project) => {
     const DynamicModel = await createDynamicModel(project.slug);
+    const collectionName = DynamicModel.collection.name;
+
     const { searchQuery, fieldFilters } = generateSearchQuery(req, project);
     const selectCount = parseInt(req.query.select) || 50;
 
-    const lastIncompleteOrders = await Order.aggregate([
-        { $unwind: '$projects' },
-        { $unwind: '$projects.entries' },
+    const alreadySelectedEntriesResult = await Order.aggregate([
         {
             $match: {
-                status: 'paid',
-                'projects.entries.totalCost': { $gt: 0 },
-                'projects.entries.costs': {
-                    $elemMatch: {
-                        totalCost: { $gt: 0 },
-                        totalOrderedCost: 0,
-                    },
-                },
+                _id: { $ne: req.query.orderId },
+                'projects.slug': project.slug,
             },
         },
         {
-            $addFields: {
-                projectExpiry: {
-                    $add: [
-                        '$createdAt',
-                        {
-                            $multiply: [
-                                '$projects.months',
-                                30 * 24 * 60 * 60 * 1000,
-                            ],
-                        },
-                        30 * 24 * 60 * 60 * 1000,
-                    ],
-                },
+            $unwind: '$projects',
+        },
+        {
+            $unwind: '$projects.entries',
+        },
+        {
+            $group: {
+                _id: null,
+                entryIds: { $addToSet: '$projects.entries.entryId' },
             },
         },
         {
             $project: {
-                _id: 1,
-                orderId: '$_id',
-                entryId: '$projects.entries.entryId',
-                lastPaid: '$createdAt',
-                selectedSubscriptions:
-                    '$projects.entries.selectedSubscriptions',
-                costs: '$projects.entries.costs',
-                expiry: '$projectExpiry',
+                _id: 0,
+                entryIds: 1,
             },
         },
-        { $sort: { lastPaid: 1 } },
     ]);
 
-    const lastOrders = await Order.aggregate([
-        { $unwind: '$projects' },
-        { $unwind: '$projects.entries' },
-        {
-            $match: {
-                'projects.entries.totalCost': { $gt: 0 },
-                'projects.entries.costs': {
-                    $not: {
-                        $elemMatch: {
-                            totalCost: { $gt: 0 },
-                            totalOrderedCost: 0,
-                        },
-                    },
-                },
-                status: 'paid',
-            },
-        },
-        {
-            $addFields: {
-                projectExpiry: {
-                    $add: [
-                        '$createdAt',
-                        {
-                            $multiply: [
-                                '$projects.months',
-                                30 * 24 * 60 * 60 * 1000,
-                            ],
-                        },
-                        30 * 24 * 60 * 60 * 1000,
-                    ],
-                },
-            },
-        },
-        {
-            $group: {
-                _id: '$projects.entries.entryId',
-                lastPaid: { $first: '$createdAt' },
-                selectedSubscriptions: {
-                    $first: '$projects.entries.selectedSubscriptions',
-                },
-                costs: {
-                    $first: '$projects.entries.costs',
-                },
-                orderId: { $first: '$_id' },
-                expiry: { $first: '$projectExpiry' },
-            },
-        },
-        { $sort: { lastPaid: 1 } },
-    ]);
+    const alreadySelectedEntries =
+        alreadySelectedEntriesResult[0]?.entryIds || [];
 
-    let paidEntryIds = lastOrders.map((order) => order._id);
-    let halfPaidEntryIds = lastIncompleteOrders.map((order) => order.entryId);
-
-    const countNotPaid = await DynamicModel.countDocuments({
-        _id: { $nin: [...paidEntryIds, ...halfPaidEntryIds] },
+    const countNotSelected = await DynamicModel.countDocuments({
+        _id: { $nin: alreadySelectedEntries },
         ...searchQuery,
     });
+
+    let entriesToPay = [];
+
+    if (countNotSelected > selectCount) {
+        entriesToPay = await DynamicModel.find({
+            _id: { $nin: alreadySelectedEntries },
+            ...searchQuery,
+        })
+            .limit(selectCount)
+            .lean();
+        return {
+            project,
+            allEntries: entriesToPay,
+        };
+    }
+
+    const {
+        nonFullySubscribed: lastIncompleteOrders,
+        fullySubscribed: lastOrders,
+    } = await getNonFullySubscribedEntries(
+        req.query.orderId,
+        project,
+        alreadySelectedEntries,
+        searchQuery,
+    );
+
+    let paidEntryIds = lastOrders.map((order) => order.entryId);
+    let halfPaidEntryIds = lastIncompleteOrders.map((order) => order.entryId);
 
     const countHalfPaid = await DynamicModel.countDocuments({
         _id: { $in: halfPaidEntryIds },
         ...searchQuery,
     });
 
-    let entriesToPay = [];
-    if (countNotPaid > selectCount) {
-        entriesToPay = await DynamicModel.find({
-            _id: { $nin: [...paidEntryIds, ...halfPaidEntryIds] },
-            ...searchQuery,
-        })
-            .limit(selectCount)
-            .lean();
-    }
+    const countNotPaid = countNotSelected;
 
     if (countNotPaid <= selectCount) {
         let entriesWithPayments = [];
@@ -350,36 +439,24 @@ const getOldestPaidEntries = async (req, project) => {
                 .lean();
         }
         if (pickFromHalfPaid > 0) {
-            halfPaidEntryIds = halfPaidEntryIds.slice(0, pickFromHalfPaid);
-            entriesWithHalfPayments = await DynamicModel.find({
-                _id: { $in: halfPaidEntryIds },
-                ...searchQuery,
-            })
-                .limit(pickFromHalfPaid)
-                .lean();
-            for (const entry of entriesWithHalfPayments) {
-                entry.oldOrders =
-                    lastIncompleteOrders.filter(
-                        (order) =>
-                            order.entryId.toString() === entry._id.toString(),
-                    ) || null;
-            }
+            entriesWithHalfPayments = lastIncompleteOrders
+                .slice(0, pickFromHalfPaid)
+                .map((payments) => {
+                    return {
+                        ...payments.entry,
+                        oldOrders: payments.oldOrders,
+                    };
+                });
         }
         if (pickFromPaid > 0) {
-            paidEntryIds = paidEntryIds.slice(0, pickFromPaid);
-            entriesWithPayments = await DynamicModel.find({
-                _id: { $in: paidEntryIds },
-                ...searchQuery,
-            })
-                .limit(pickFromPaid)
-                .lean();
-            for (const entry of entriesWithPayments) {
-                entry.oldOrders =
-                    lastOrders.filter(
-                        (order) =>
-                            order._id.toString() === entry._id.toString(),
-                    ) || null;
-            }
+            entriesWithPayments = lastOrders
+                .slice(0, pickFromPaid)
+                .map((payments) => {
+                    return {
+                        ...payments.entry,
+                        oldOrders: payments.oldOrders,
+                    };
+                });
         }
         entriesToPay = [
             ...entriesWithOutPayments,
@@ -405,9 +482,9 @@ const makeProjectForOrder = (project, allEntries) => {
                     .filter((field) => {
                         const isSubscriptionValid =
                             field.subscription &&
-                            entry[field.name] !== undefined;
+                            entry[field.name];
 
-                        if (!entry.oldOrders) {
+                        if (!entry.oldOrders || !isSubscriptionValid) {
                             return isSubscriptionValid;
                         }
 
@@ -430,6 +507,8 @@ const makeProjectForOrder = (project, allEntries) => {
         }),
     };
 };
+
+const orderIsValid = async (req, res) => {};
 
 module.exports = {
     getOldestPaidEntries,
